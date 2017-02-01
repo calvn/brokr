d0fdb099004a3eba4f4ca9ec48d15cfe59381bcb
Calvin Leung Huang <cleung2010@gmail.com>
2017-01-31 21:28:16 -0500

    Fix short and cover commands

--- cmd/short.go
+++ cmd/short.go
@@ -21,32 +21,20 @@ import (
 )
 
 // shortCmd represents the short command
-var shortCmd = &cobra.Command{
-	Use:   "short",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
+func newShortCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "short",
+		Short: "A brief description of your command",
+		Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Work your own magic here
-		fmt.Println("short called")
-	},
-}
-
-func init() {
-	RootCmd.AddCommand(shortCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// shortCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// shortCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
+		Run: func(cmd *cobra.Command, args []string) {
+			// TODO: Work your own magic here
+			fmt.Println("short called")
+		},
+	}
+	return cmd
 }
